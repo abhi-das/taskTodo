19e24e62be934fed3c595c547b3d46ef1adcfc32
Das <s7527620@intl.bns>
2018-01-11 15:38:41 -0500

    update

--- src/client/src/app/services/tasks.service.ts
+++ src/client/src/app/services/tasks.service.ts
@@ -15,29 +15,26 @@ export class TasksService {
   domain: string = 'http://localhost:3000';
 
   getTasks(): Observable< any > {
-  	return this._httpClient.get<Task[]>(`${this.domain}/api/tasks`)
-  		.map(res => res);
+
+  	return this._httpClient.get<Task[]>(`${this.domain}/api/tasks`);
+
   }
 
   addTask(task: Task): Observable< any > {
 
-  	return this._httpClient.post<Task>(`${this.domain}/api/tasks`, task)
-  		.map(res => res);
+  	return this._httpClient.post<Task>(`${this.domain}/api/tasks`, task);
+
   }
 
   deleteTask(id:string): Observable< any > {
-  	// console.log('ser>>>>>>',id);
-  	return this._httpClient.delete(`${this.domain}/api/tasks/${id}`)
-  		.map(res => res);
+
+  	return this._httpClient.delete(`${this.domain}/api/tasks/${id}`);
 
   }
 
   updateTask(task: Task): Observable< any > {
 
-  	return this._httpClient.put<Task>(`${this.domain}/api/tasks/${task['_id']}`, task)
-  		.map(res => res);
+  	return this._httpClient.put<Task>(`${this.domain}/api/tasks/${task['_id']}`, task);
 
   }
-
-
 }
--- src/client/src/app/tasks/tasks.component.ts
+++ src/client/src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { TasksService } from '../services/tasks.service';
 import { Task } from '../models/task.module';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { FormGroup, FormBuilder } from '@angular/forms';
 
@@ -14,6 +15,13 @@ export class TasksComponent implements OnInit {
 
 	addTaskForm: FormGroup;
 	taskList: Task[];
+	errMsg: any = {
+		isError: false,
+		getTaskErr: {},
+		newTaskErr: {},
+		editTaskErr: {},
+		deleteTaskErr: {}
+	};
 
 	constructor(private _taskSrv: TasksService, private _formBd: FormBuilder) { }
 
@@ -29,45 +37,134 @@ export class TasksComponent implements OnInit {
 
 	getTaskList() {
 
-		this._taskSrv.getTasks().subscribe(result => {
-			this.taskList = result;
-		});
+		this.errMsg.isError = false;
+		this._taskSrv.getTasks().subscribe(
+			(result) => {
+				this.taskList = result;
+			},
+			(err : HttpErrorResponse) => {
 
+				this.errMsg.isError = true;
+
+				if(err.error instanceof Error) {
+
+					//A client-side or network error occurred.
+		 			this.errMsg.getTaskErr.message = err.error.message; 
+					
+				} else {
+					
+					//Backend returns unsuccessful response codes such as 404, 500 etc.
+					// console.log(err);
+					this.errMsg.getTaskErr.message = err.message;
+				}
+			}
+		);
 	}
 
 	onNewTaskSubmit() {
 
-		this._taskSrv.addTask(this.addTaskForm.value).subscribe(res => {
-			this.taskList.push(res);
-			this.addTaskForm.reset();
-		})
+		this.errMsg.isError = false;
 
+		this._taskSrv.addTask(this.addTaskForm.value).subscribe(
+			(res) => {
+
+				// Option 1:
+				// this.taskList.push(res);
+				
+				// Option 2:
+				this.getTaskList();
+
+				this.addTaskForm.reset();
+			},
+			(err: HttpErrorResponse) => {
+
+				this.errMsg.isError = true;
+
+				if(err.error instanceof Error) {
+
+					//A client-side or network error occurred.
+		 			this.errMsg.newTaskErr.message = err.error.message; 
+					
+				} else {
+					
+					//Backend returns unsuccessful response codes such as 404, 500 etc.
+					// console.log('onNew > ',err);
+					this.errMsg.newTaskErr.message = err.message;
+				}
+			}
+		);
 	}
 
 	onTaskDelete(id: string) {
-		this._taskSrv.deleteTask(id).subscribe(res => {
-			
-			if(res['ok'] == '1') {
 
-				let deletedItm = this.taskList.filter(itm => {
-					return itm['_id'] === id;
-				})
+		this.errMsg.isError = false;
 
-				let deleteItmIdx = this.taskList.indexOf(deletedItm[0])
-				this.taskList.splice(deleteItmIdx, 1);
+		this._taskSrv.deleteTask(id).subscribe(
+			(res) => {
+
+				if(res['ok'] == '1') {
+
+					// Option 1:
+					// let deletedItm = this.taskList.filter(itm => {
+					// 	return itm['_id'] === id;
+					// })
+
+					// let deleteItmIdx = this.taskList.indexOf(deletedItm[0])
+					// this.taskList.splice(deleteItmIdx, 1);
+					
+					// Option 2:
+					this.getTaskList();
+				} else {
+
+				}
+			},
+			(err: HttpErrorResponse) => {
+
+				this.errMsg.isError = true;
+
+				if(err.error instanceof Error) {
+
+					//A client-side or network error occurred.
+		 			this.errMsg.deleteTaskErr.message = err.error.message; 
+					
+				} else {
+					
+					//Backend returns unsuccessful response codes such as 404, 500 etc.
+					// console.err('Server could not delete task.', err);
+					this.errMsg.deleteTaskErr.message = err.message;
+				}
 			}
-		})
+		);
 	}
 
 	onEditTask(task: Task, $event: any) {
 
 		var editTask = task;
+		
 		editTask['isDone'] = $event.target.checked.toString();
 
-		this._taskSrv.updateTask(editTask).subscribe(res => {
-			// console.log(res);
-		})
+		this.errMsg.isError = false;
 
+		this._taskSrv.updateTask(editTask).subscribe(
+			(res) => {
+				// console.log(res);
+			},
+			(err: HttpErrorResponse) => {
+
+				this.errMsg.isError = true;
+
+				if(err.error instanceof Error) {
+
+					//A client-side or network error occurred.
+		 			this.errMsg.editTaskErr.message = err.error.message; 
+					
+				} else {
+					
+					//Backend returns unsuccessful response codes such as 404, 500 etc.
+					// console.err('Server could not edit task.', err);
+					this.errMsg.editTaskErr.message = err.message;
+				}
+			}
+		);
 	}
-
 }
